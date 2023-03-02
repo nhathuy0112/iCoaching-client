a5b9b2668e72160869ea302a53e8ac8be8f3fb2a
Le Minh Hoang Tran <82258999+tranleminhhoang2410@users.noreply.github.com>
2023-03-03 01:44:31 +0700

    handle get certificate images

--- src/pages/Coach/Verify/Verify.jsx
+++ src/pages/Coach/Verify/Verify.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import classNames from 'classnames/bind';
 import styles from './Verify.module.scss';
 
@@ -6,13 +6,18 @@ import ImageUploading from 'react-images-uploading';
 import { AiOutlinePlus } from 'react-icons/ai';
 import { BiTrash } from 'react-icons/bi';
 import { MdOutlineEdit } from 'react-icons/md';
-import { useDispatch } from 'react-redux';
-import { certificationSubmitAsync } from '~/features/coachSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { certificationSubmitAsync, getCertificationAsync } from '~/features/coachSlice';
+import SuccessMessage from '~/components/SuccessMessage';
 
 const cx = classNames.bind(styles);
 
 const Verify = () => {
     const dispatch = useDispatch();
+    const { certificationImages, status, message } = useSelector((state) => state.coach);
+    console.log('images: ', certificationImages);
+    console.log('status: ', status);
+
     const [images, setImages] = useState([]);
     const maxNumber = 69;
 
@@ -22,8 +27,12 @@ const Verify = () => {
         setImages(imageList);
     };
 
+    useEffect(() => {
+        dispatch(getCertificationAsync());
+    }, [dispatch]);
+
     const handleSubmit = () => {
-        dispatch(certificationSubmitAsync({ file: images }));
+        dispatch(certificationSubmitAsync({ files: images }));
     };
 
     return (
@@ -34,65 +43,78 @@ const Verify = () => {
                     <p>Bạn cần xác minh mình là Huấn luyện viên trước khi tham gia huấn luyện cho khách hàng !</p>
                     <p>Vui lòng cập nhật ít nhất một ảnh của chứng chỉ huấn luyện !</p>
                 </div>
-                <div className={cx('image-wrapper')}>
-                    <ImageUploading
-                        multiple
-                        value={images}
-                        onChange={onChange}
-                        maxNumber={maxNumber}
-                        dataURLKey="data_url"
-                    >
-                        {({
-                            imageList,
-                            onImageUpload,
-                            onImageRemoveAll,
-                            onImageUpdate,
-                            onImageRemove,
-                            isDragging,
-                            dragProps,
-                        }) => (
-                            // write your building UI
-                            <div className={cx('image-upload')}>
-                                <div className={cx('action')}>
-                                    <button
-                                        id={cx('add-btn')}
-                                        style={isDragging ? { color: 'red' } : undefined}
-                                        onClick={onImageUpload}
-                                        {...dragProps}
-                                    >
-                                        <AiOutlinePlus className={cx('icon')} />
-                                        <span>Thêm ảnh</span>
-                                    </button>
-                                    <button id={cx('remove-all-btn')} onClick={onImageRemoveAll}>
-                                        <BiTrash className={cx('icon')} />
-                                        Xóa tất cả
-                                    </button>
-                                </div>
-                                <div className={cx('image-list')}>
-                                    {imageList.map((image, index) => (
-                                        <div key={index} className={cx('image-item')}>
-                                            <img src={image['data_url']} alt="" width="100" />
-                                            <div className={cx('image-action')}>
-                                                <button id={cx('update-btn')} onClick={() => onImageUpdate(index)}>
-                                                    <MdOutlineEdit />
-                                                </button>
-                                                <button id={cx('remove-btn')} onClick={() => onImageRemove(index)}>
-                                                    <BiTrash />
-                                                </button>
-                                            </div>
-                                        </div>
-                                    ))}
-                                </div>
+                {status === 'Pending' ? (
+                    <div className={cx('image-wrapper')}>
+                        <div className={cx('image-upload')}>
+                            <div className={cx('image-list')}>
+                                {certificationImages.map((image) => (
+                                    <div key={image} className={cx('image-item')}>
+                                        <img src={image} alt="" width="100" />
+                                    </div>
+                                ))}
                             </div>
-                        )}
-                    </ImageUploading>
-                </div>
+                        </div>
+                    </div>
+                ) : (
+                    <div className={cx('image-wrapper')}>
+                        <ImageUploading
+                            multiple
+                            value={images}
+                            onChange={onChange}
+                            maxNumber={maxNumber}
+                            dataURLKey="data_url"
+                        >
+                            {({
+                                imageList,
+                                onImageUpload,
+                                onImageRemoveAll,
+                                onImageUpdate,
+                                onImageRemove,
+                                isDragging,
+                                dragProps,
+                            }) => (
+                                // write your building UI
+                                <div className={cx('image-upload')}>
+                                    <div className={cx('action')}>
+                                        <button
+                                            id={cx('add-btn')}
+                                            style={isDragging ? { color: 'red' } : undefined}
+                                            onClick={onImageUpload}
+                                            {...dragProps}
+                                        >
+                                            <AiOutlinePlus className={cx('icon')} />
+                                            <span>Thêm ảnh</span>
+                                        </button>
+                                        <button id={cx('remove-all-btn')} onClick={onImageRemoveAll}>
+                                            <BiTrash className={cx('icon')} />
+                                            Xóa tất cả
+                                        </button>
+                                    </div>
+                                    <div className={cx('image-list')}>
+                                        {imageList.map((image, index) => (
+                                            <div key={index} className={cx('image-item')}>
+                                                <img src={image['data_url']} alt="" width="100" />
+                                                <div className={cx('image-action')}>
+                                                    <button id={cx('update-btn')} onClick={() => onImageUpdate(index)}>
+                                                        <MdOutlineEdit />
+                                                    </button>
+                                                    <button id={cx('remove-btn')} onClick={() => onImageRemove(index)}>
+                                                        <BiTrash />
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>
+                            )}
+                        </ImageUploading>
+                    </div>
+                )}
             </div>
-            {images && (
-                <button id={cx('send-btn')} onClick={handleSubmit}>
-                    Gửi
-                </button>
-            )}
+            {message && <SuccessMessage message={message} />}
+            <button id={cx('send-btn')} className={status === 'Pending' && cx('disabled')} onClick={handleSubmit}>
+                Gửi
+            </button>
         </div>
     );
 };
