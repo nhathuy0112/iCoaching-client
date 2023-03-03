4c374ed3b83c2d982cdd976d37b0093a9c911c8a
hieukhanhng <hieukhanh.ng@gmail.com>
2023-03-03 20:02:31 +0700

    #14 Khanh update Admin homepage api

--- src/app/store.js
+++ src/app/store.js
@@ -3,23 +3,17 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '~/features/userSlice';
 import guestReducer from '~/features/guestSlice';
 import coachReducer from '~/features/coachSlice';
-<<<<<<< HEAD
 import adminReducer from '~/features/adminSlice'
-=======
 import superAdminSlice from '~/features/superAdminSlice';
->>>>>>> fd94aed4959cda68de3c895a34dfaa8f2eb30f47
 
 const rootReducer = {
     user: userReducer,
     guest: guestReducer,
     coach: coachReducer,
-<<<<<<< HEAD
     admin: adminReducer,
-}
-=======
     superadmin: superAdminSlice,
+
 };
->>>>>>> fd94aed4959cda68de3c895a34dfaa8f2eb30f47
 
 export const store = configureStore({
     reducer: rootReducer,
